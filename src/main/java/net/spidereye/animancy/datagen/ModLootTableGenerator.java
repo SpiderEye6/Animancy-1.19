fbd9fc0bfe7bbf378c6f216d25a993fc5bc2b0cf
Spidereye <harrisonzrede@gmail.com>
2026-06-16 12:41:43 -0500

    Implemented Datagen, configured build for 1.19.2

---
+++ src/main/java/net/spidereye/animancy/datagen/ModLootTableGenerator.java
@@ -0,0 +1,20 @@
+package net.spidereye.animancy.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.SimpleFabricLootTableProvider;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.context.LootContextTypes;
+import net.minecraft.util.Identifier;
+
+import java.util.function.BiConsumer;
+
+public class ModLootTableGenerator extends SimpleFabricLootTableProvider {
+    public ModLootTableGenerator(FabricDataGenerator dataGenerator) {
+        super(dataGenerator, LootContextTypes.BLOCK);
+    }
+
+    @Override
+    public void accept(BiConsumer<Identifier, LootTable.Builder> identifierBuilderBiConsumer) {
+
+    }
+}
